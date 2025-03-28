14ea101553e9984bae572d0ea49a8035d0f6fdd9
Alexander Kettler <alexander.kettler91@gmail.com>
2025-03-28 18:26:51 +0100

    feat: add episode count to the bottom sheet

--- poddycast_discover/lib/features/search/presentation/widgets/podcast_sheet_content.dart
+++ poddycast_discover/lib/features/search/presentation/widgets/podcast_sheet_content.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:podcast_search/podcast_search.dart';
 import 'package:poddycast_discover/features/search/presentation/provider/audio_feed_provider.dart';
 
-class PodcastSheetContent extends StatelessWidget {
+class PodcastSheetContent extends StatefulWidget {
   final String feedUrl;
 
   const PodcastSheetContent({super.key, required this.feedUrl});
+
+  @override
+  State<PodcastSheetContent> createState() => _PodcastSheetContentState();
+}
+
+class _PodcastSheetContentState extends State<PodcastSheetContent> {
+  late Future<Podcast> futurePodcastFeed;
+  int episodeCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePodcastFeed = Podcast.loadFeed(url: widget.feedUrl);
+    futurePodcastFeed.then((podcast) {
+      setState(() => episodeCount = podcast.episodes.length);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +37,16 @@ class PodcastSheetContent extends StatelessWidget {
             children: [
               Icon(Icons.manage_search_outlined),
               Icon(Icons.favorite_border_outlined),
+              AnimatedFlipCounter(
+                duration: Duration(microseconds: 500),
+                prefix: 'Episodes:',
+                value: episodeCount,),
             ],
           ),
         ),
         Expanded(
           child: FutureBuilder<Podcast>(
-            future: Podcast.loadFeed(url: feedUrl),
+            future: futurePodcastFeed,
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
